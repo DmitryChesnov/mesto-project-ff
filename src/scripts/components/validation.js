605f32a59621f46d8e976d304cc9935a46bda6d7
DmitryChesnov <advokat33333@gmail.com>
2025-06-05 15:53:40 +0300

    2REVUE-module-js-final

--- src/scripts/components/validation.js
+++ src/scripts/components/validation.js
@@ -11,36 +11,38 @@ function hideInputError(formElement, inputElement, settings) {
   errorElement.textContent = "";
   errorElement.classList.remove(settings.errorClass);
 }
+// заморочился с созданием функции получения кастомной ошибки, а то все показывало браузерные и не соответствовало макету
+function getCustomErrorMessage(inputElement) {
+  if (inputElement.validity.valueMissing) {
+    return inputElement.dataset.errorRequired || inputElement.validationMessage;
+  }
+  if (inputElement.validity.tooShort) {
+    if (inputElement.dataset.errorMinlength) {
+      const length = inputElement.value.length;
+      const ending = length === 1 ? "" : "а";
+      return inputElement.dataset.errorMinlength
+        .replace("{length}", length)
+        .replace("{ending}", ending);
+    }
+    return inputElement.validationMessage;
+  }
+  if (inputElement.validity.patternMismatch) {
+    return inputElement.dataset.errorPattern || inputElement.validationMessage;
+  }
+  if (inputElement.validity.typeMismatch && inputElement.type === "url") {
+    return inputElement.dataset.errorMessage || inputElement.validationMessage;
+  }
+  return inputElement.validationMessage;
+}
 
 function checkInputValidity(formElement, inputElement, settings) {
   if (!inputElement.validity.valid) {
-    let errorMessage = "";
-
-    if (inputElement.validity.valueMissing) {
-      errorMessage = inputElement.dataset.errorRequired;
-    } else if (inputElement.validity.patternMismatch) {
-      errorMessage = inputElement.dataset.errorPattern;
-    } else if (inputElement.validity.tooShort) {
-      const currentLength = inputElement.value.length;
-      const ending = currentLength === 1 ? "" : "а";
-      errorMessage = inputElement.dataset.errorMinlength
-        .replace("{length}", currentLength)
-        .replace("{ending}", ending);
-    } else if (
-      inputElement.validity.typeMismatch &&
-      inputElement.type === "url"
-    ) {
-      errorMessage = inputElement.dataset.errorType;
-    } else {
-      errorMessage = inputElement.validationMessage;
-    }
-
+    const errorMessage = getCustomErrorMessage(inputElement);
     showInputError(formElement, inputElement, errorMessage, settings);
   } else {
     hideInputError(formElement, inputElement, settings);
   }
 }
-
 function hasInvalidInput(inputList) {
   return inputList.some((inputElement) => {
     return !inputElement.validity.valid;
